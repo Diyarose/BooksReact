3c6789fbf760ec2c70d593342840e1153e493aef
Diyarose <dhiyarosedavis@gmail.com>
2024-06-07 14:15:37 +0530

    updates done

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -9,7 +9,7 @@ const AddBook = () => {
             <h1><center><i>BOOK REGISTRATION</i></center></h1>
             <div className="row g-3">
                 <div className="col-col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row">
+                    <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-6">
                             <label htmlFor="" className="form-label">Book Title</label>
                             <input type="text" className="form-control" />
